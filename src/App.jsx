eaac3fc3a1d1e8f6ff48a657c312e6e6e4ba9c01
Kirkland Brown <kizzet373@gmail.com>
2026-05-27 13:22:58 -0400

    Add Speed Check mini-game with difficulty configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { AppShell } from './components/layout';
-import { AttacksView, ItemsView, PlayersView, PokemonView, TournamentsView } from './components/views';
+import { AttacksView, ItemsView, PlayersView, PokemonView, SpeedCheckView, TournamentsView } from './components/views';
 
 export function App() {
   return (
@@ -13,6 +13,7 @@ export function App() {
         <Route path="/moves" element={<AttacksView />} />
         <Route path="/players" element={<PlayersView />} />
         <Route path="/tournaments" element={<TournamentsView />} />
+        <Route path="/speed-check" element={<SpeedCheckView />} />
         <Route path="*" element={<Navigate to="/pokemon" replace />} />
       </Routes>
     </AppShell>
--- src/components/views/index.js
+++ src/components/views/index.js
@@ -2,4 +2,5 @@ export { AttacksView } from './AttacksView';
 export { ItemsView } from './ItemsView';
 export { PlayersView } from './PlayersView';
 export { PokemonView } from './PokemonView';
+export { SpeedCheckView } from './SpeedCheckView';
 export { TournamentsView } from './TournamentsView';
